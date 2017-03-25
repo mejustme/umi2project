26a14204e3ac16724a1ef136f6f959c45bf87019
陈钦辉 <hzchenqinhui@corp.netease.com>
2017-03-25 20:56:27 +0800

    @dev run nie

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 var fs = require('fs');
+var exec = require('child_process').exec;
+var execSync = require('child_process').execSync;
+
 var readFile = function (path) {
     return  new Promise(function (resolve, reject) {
         fs.readFile(path,'utf8',function (err, data) {
@@ -13,6 +16,7 @@ var readFile = function (path) {
 
 // 解析文本UIM
 var parse2Tree = function (data) {
+    data = data.replace(/\n/g,'');
     var tree = {
             name: 'root',
             children: []
@@ -24,15 +28,15 @@ var parse2Tree = function (data) {
         parentName= 'root';
         for(var i=0; i<items.length; i++){
             if(!items[i] || items[i] === 'm') continue;
-            traverse (tree, parentName, items[i]);
+            traverseBuild (tree, parentName, items[i]);
             parentName = items[i];
         }
     });
-    console.dir(tree);
+    return  tree.children[0]; //返回真正的路径树
 };
 
 // 遍历树，找到父节点，如果孩子节点不存在，则插入孩子节点
-var traverse = function (tree, parentName, name) {
+var traverseBuild = function (tree, parentName, name) {
     var i;
     if(tree.name == parentName){
         var exist = false;
@@ -48,11 +52,47 @@ var traverse = function (tree, parentName, name) {
         return true;
     }
     for(i=0; i<tree.children.length; i++){
-         if(traverse(tree.children[i], parentName, name)) return true;
+        if(traverseBuild(tree.children[i], parentName, name)) return true;
     }
 };
 
+var traverse = function (obj, callback) {
+    var tempTree = obj.tree;
+    var tempPath = obj.path;
+    callback(obj);
+    for(var i=0; i<tempTree.children.length; i++){
+        obj.tree = tempTree.children[i];
+        obj.path = tempPath + "/" + obj.tree.name;
+        traverse(obj, callback);
+    }
+};
+
+var buildModule = function (obj) {
+    var cmd = "nei build -sk 3c0776c04ad289211f2987f78737873c -module {moduleName} -name {path} -author {author}".replace(/{(.+?)}/g, function ($0,$1) {
+        return obj[$1];
+    });
+
+    console.log(cmd);
+    exec(cmd, function(error, stdout, stderr) {
+        if (error) {
+            console.error(error);
+            return;
+        }
+        console.log(stdout);
+        console.log(stderr);
+    });
+};
+
+
+
 readFile('./config.txt').then(function (data) {
-    parse2Tree(data);
+    var tree = parse2Tree(data);
+    var moduleName = tree.name;
+    traverse({
+        tree: tree,
+        moduleName: moduleName,
+        path: moduleName,
+        author: "hzchenqinhui@corp.netease.com"
+    },buildModule);
 });
 
